b8301788d677a69b73ddc1dd4f8931bb766c7455
AmanKumarVerma11 <akverma11aug2002@gmail.com>
2022-07-16 23:01:03 +0530

    Added Butterfly number pattern

---
+++ ButterflyPattern.c
@@ -0,0 +1,43 @@
+/* Butterfly pattern
+    n=5
+    1                  1
+    1 2              2 1
+    1 2 3          3 2 1
+    1 2 3 4      4 3 2 1
+    1 2 3 4 5  5 4 3 2 1
+    1 2 3 4      4 3 2 1
+    1 2 3          3 2 1
+    1 2              2 1
+    1                  1
+*/
+#include <stdio.h>
+
+int main() {
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=1;i<n;i++) {
+        for(j=1;j<=i;j++) {
+            printf("%d ",j);          
+        }
+        for(j=1;j<=2*n-2*i;j++) {
+            printf("  ");
+        }
+        for(j=i;j>=1;j--) {
+            printf(" %d",j);          
+        }
+        printf("\n");
+    }
+    for(i=n;i>=1;i--) {
+        for(j=1;j<=i;j++) {
+            printf("%d ",j);           
+        }
+        for(j=1;j<=2*n-2*i;j++) {
+            printf("  ");
+        }
+        for(j=i;j>=1;j--) {
+            printf(" %d",j);          
+        }
+        printf("\n");
+    }
+    return 0;
+}
